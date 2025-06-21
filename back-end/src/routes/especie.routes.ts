50fe16cc8278d16b9ce88c3a3e6226cfc9bd644c
Lucas M <lucasmateustds@gmail.com>
2025-06-21 10:03:19 -0300

    :boom: fix: Revertendo arquivos adicionados incorretamente

--- back-end/src/routes/especie.routes.ts
+++ back-end/src/routes/especie.routes.ts
@@ -1,5 +1,20 @@
 import { Elysia } from 'elysia';
 import { getEspecies } from '../controllers/especie.controller';
 
+/**
+ * Roteador Elysia responsável pelos endpoints de especie.
+ * Todas as rotas definidas aqui usam o prefixo `/especies`.
+ *
+ * @constant
+ * @type {Elysia}
+ */
 export const especieRoutes = new Elysia({ prefix: '/especies' })
+    /**
+     * Rota para obter uma lista de todas as espécies.
+     *
+     * @name GET /especies
+     * @function
+     * @memberof especieRoutes
+     * @returns {Promise<Array<object>>} Uma promessa que resolve para um array de objetos de espécies.
+     */
     .get('/', async () => await getEspecies());
