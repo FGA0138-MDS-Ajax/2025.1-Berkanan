b21ba53cb8c4076bb48b8d062623efe53959de24
Lucas M <lucasmateustds@gmail.com>
2025-06-21 09:41:25 -0300

    Revert "Método de salvar espécies"
    This reverts commit 1ad69c94ea92a1afec51a283964a3d96b836f2f9.

--- back-end/src/controllers/especie.controller.ts
+++ back-end/src/controllers/especie.controller.ts
@@ -1,9 +1,5 @@
-import { getAllEspecies, saveEspecie, type Especie } from '../models/especie.model';
+import { getAllEspecies } from '../models/especie.model';
 
 export const getEspecies = async () => {
     return await getAllEspecies();
-};
-
-export const salvarEspecies = async (especie: Especie) => {
-    return await saveEspecie(especie);
 };
